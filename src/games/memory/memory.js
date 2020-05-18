ad69aec9aacd9cf78c336c780fa42532494f4966
dongsohu <32343603+dongsohu@users.noreply.github.com>
2020-05-18 18:41:16 -0400

    Css
    Some css was done

--- src/games/memory/memory.js
+++ src/games/memory/memory.js
@@ -9,6 +9,23 @@ export default class Memory extends GameComponent {
       chats: [],
       input: ""
     };
+
+    var allIds = this.getSessionUserIds();
+    
+
+    // Store the username of the host (aka player 1)
+var host = this.getSessionCreatorUserId();
+
+
+    // Store the username of the guest (aka player 2)
+    var guest;
+
+   for(var i = 0; i++; i< allIds.length){
+     if(allIds[i] !== host){
+      console.log("found the guest");
+     }
+      // console.log(allIds[i =! creator]);
+   }
   }
 
   gameState = {
@@ -31,31 +48,69 @@ export default class Memory extends GameComponent {
     var users = this.getSessionUserIds().map(user_id => (
       <li key={user_id}>{user_id}</li>
     ));
-    var creator = this.getSessionCreatorUserId();
+    var host = this.getSessionCreatorUserId();
     // var funVar = "the other variable";
     var message;
-    if (id === creator) {
+    if (id === host) {
       message = "I am a host";
     } else {
       message = "I am a guest";
     }
+
+    const container = {
+
+      display: "flex",
+      flexGrow: "1",
+      backgroundColor: "blue",
+      flexFlow: "column",
+      /* flex-basis: 100%; */
+      height: "-webkit-fill-available"
+    };
+    const gameInfo = {
+      border: "solid",
+      display: "flex",
+      // flex: "1",
+      backgroundColor: "green",
+      flex: "wrap", 
+      flexFlow: "column",
+      width: 600,
+      margin: 15,
+    };
+    const scoreboard = {
+      display: "flex",
+      backgroundColor: "red",
+      justifyContent: "center",
+      flexFlow: "column",
+      alignContent: "center",
+      flex: "wrap", 
+      width: 100,
+      margin: 15,
+    };
     return (
-      <div>
-        <div>
+      <div style={container} id="container">
+        <div style={gameInfo} id="gameInfo">
+          <div>
           <p>Session ID: {id}</p>
-          <p>Session creator: {creator}</p>
+          </div>
+          <div>
+          <p>Session creator: {host}</p>
+          </div>
+          <div>
           <p>Session users:</p>
           <ul>{users}</ul>
+          </div>
         </div>
         <div>
           <div>{message}</div>
           {/* <span>The value of funVar is {funVar}</span> */}
         </div>
-        <div>
-          <span>Player One: {this.gameState.playerOneScore}</span>
-        </div>
-        <div>
-          <span>Player Two: {this.gameState.playerTwoScore}</span>
+        <div style={scoreboard} id="scoreboard">
+          <div>
+            <span>Player One: {this.gameState.playerOneScore}</span>
+          </div>
+          <div>
+            <span>Player Two: {this.gameState.playerTwoScore}</span>
+          </div>
         </div>
       </div>
       // <div>
